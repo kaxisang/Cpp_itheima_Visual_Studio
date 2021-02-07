ba1a3e6fe2d9452d18b733aeba62fd44d9e0edab
WangLuxiang <981835013@qq.com>
2021-02-07 17:32:44 +0800

    Update

--- code/04 Flow Structure/05_Switch_Statement.cpp
+++ code/04 Flow Structure/05_Switch_Statement.cpp
@@ -23,18 +23,45 @@
  *			}
  *		}
  */
+#include <iostream>
+using namespace std;
 
 /**
  * Raye the movie
  * 10~9 Classical movie
  * 8~7  Very good
- * 6~5  Nothing special
+ * 6~5  Ordinary
  * 5~0  Bad movie
  */
 
 int main() {
 	auto score = 0;
-	cout << 
+	cout << "Please rate the movie: ";
+	cin >> score;
+	cout << "Your score: " << score << endl;
+
+	// Switch Statement must have break statement
+	switch (score) {
+		case 10: 
+		case 9: {
+			cout << "You think this is a classic movie" << endl;
+			break;
+		}
+		case 8:
+		case 7: {
+			cout << "You think this movie is very good" << endl;
+			break;
+		}
+		case 6:
+		case 5: {
+			cout << "You think this movie is ordinary" << endl;
+			break;
+		}
+		default: {
+			cout << "You think this is a bad movie" << endl;
+			break;
+		}
+	}
 
 	return 0;
 }
