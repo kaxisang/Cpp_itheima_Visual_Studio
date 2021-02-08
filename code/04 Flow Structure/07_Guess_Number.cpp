1ce07823ada1666aa0c4e07bc8ffd00a2419ae13
WangLuxiang <981835013@qq.com>
2021-02-08 20:32:46 +0800

    Update

---
+++ code/04 Flow Structure/07_Guess_Number.cpp
@@ -0,0 +1,26 @@
+/**************************************************
+* File name:     07_Guess_Number.cpp
+* Created time:  02/07/2021 5:34:02 PM
+* Author:        Wang Luxiang
+* Version:		 v0.1.0
+**************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Guessing Numbers Game
+
+    // System generate random numbers 
+
+    // Player guess numbers
+
+    // Judge the player's guess
+        // Player's guessing is right
+            // Exit the game
+
+        // Player's guessing is wrong
+            // Guessing a again
+
+    return 0;
+}
