2fb65ce2f2d0de24dc1fd8bd42136fbce7d7541c
WangLuxiang <981835013@qq.com>
2021-05-03 16:01:07 +0800

    update

--- code/03 C++核心编程/代码/03 函数提高/02_函数的占位参数.cpp
+++ code/03 C++核心编程/代码/03 函数提高/02_函数的占位参数.cpp
@@ -10,7 +10,14 @@ using namespace std;
 // 函数的形参列表中可以有占位参数，用来作占位，调用函数时必须填补该位置
 // 语法：返回值类型 函数名 (数据类型) {}
 
+// 占位参数
+// 占位参数可以有默认值
+void func(int a, int = 10 ) {
+    cout << "func" << endl;
+}
+
 int main() {
+    func(10, 10);
 
     return 0;
 }
---
+++ code/03 C++核心编程/代码/03 函数提高/03_函数重载.cpp
@@ -0,0 +1,69 @@
+﻿/**************************************************
+* File name:     03_函数重载.cpp
+* Created time:  2021/5/3 15:00:53
+* Author:        Wang Luxiang
+* Version:		 v0.1.0
+**************************************************/
+#include <iostream>
+using namespace std;
+
+// 函数名相同，提高复用性
+// 满足条件
+    // 同一个作用域下
+    // 函数名称相同
+    // 函数参数类型不同或者个数不同或者顺序不同
+// 函数的返回值不可以作为函数重载的条件
+
+void func() {
+    cout << "func 的调用" << endl;
+}
+
+void func(int a) {
+    cout << "func (int a)的调用" << endl;
+    cout << a << endl;
+}
+
+void func(int a, int b) {
+    ;;
+}
+
+//void func(int b, int a) {
+//    ;;
+//}
+
+
+/////////////////函数重载注意事项/////////////////
+// 引用作为重载的条件
+void funcA(int& a) {
+    cout << "funcA(int& a)" << endl;
+}
+
+void funcA(const int& a) {
+    cout << "funcB(int& b)" << endl;
+}
+
+// 函数重载与默认参数
+void funcB(int a) {
+    cout << "funcB(int a)" << endl;
+}
+
+void funcB(int a, int b = 10) {
+    cout << "funcB(int a = 10)" << endl;
+}
+
+int main() {
+    // 函数重载
+    func();
+    func(10);
+    func(10, 10);
+
+    // 函数重载注意事项
+    int a = 10;
+    funcA(a);  // 引用
+    funcA(10); // 常量引用，此处的10属于字面常量
+
+    funcB(20, 20);
+    // funcB(20); 程序出现二义性
+
+    return 0;
+}
