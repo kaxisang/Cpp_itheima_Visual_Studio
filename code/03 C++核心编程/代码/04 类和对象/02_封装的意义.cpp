82170e3297ca276ea33143488c539ffba228d831
WangLuxiang <981835013@qq.com>
2021-05-05 22:14:10 +0800

    update

---
+++ code/03 C++核心编程/代码/04 类和对象/02_封装的意义.cpp
@@ -0,0 +1,42 @@
+﻿/**************************************************
+* File name:     02_封装的意义.cpp
+* Created time:  2021/5/5 21:00:00
+* Author:        Wang Luxiang
+* Version:		 v0.1.0
+**************************************************/
+
+// 封装是C++面向对象三大特性之一
+// 封装的意义：
+    // 将属性和行为作为一个整体，表现生活中的事物
+    // 将属性和行为加以权限的控制
+// 在设计类的时候，属性和行为写在一起，表现事物
+// 语法：class
+
+// 设计一个圆类，求圆的周长
+// 圆周长的公式：2*PI*r
+
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14159;
+
+class Circle {
+    // 访问权限
+public:
+    // 属性
+    int r;
+    // 行为
+    double calculateC() {
+        return 2 * PI * r;
+    }
+};
+
+int main() {
+    // 通过圆类创建一个圆，创建对象
+    // 实例化
+    Circle my_circle;
+    cout << "圆的半径为：";
+    cin >> my_circle.r;
+    cout << "圆的周长为：" << my_circle.calculateC() << endl;
+    return 0;
+}
---
+++ code/03 C++核心编程/代码/04 类和对象/03_学生类.cpp
@@ -0,0 +1,34 @@
+﻿/**************************************************
+* File name:     03_学生类.cpp
+* Created time:  2021/5/5 22:05:52
+* Author:        Wang Luxiang
+* Version:		 v0.1.0
+**************************************************/
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    string id;
+
+    void SetInfo() {
+        cout << "学生姓名：";
+        cin >> name;
+        cout << "学生学号：";
+        cin >> id;
+        cout << "设置成功！" << endl;
+    }
+
+    void ShowInfo() {
+        cout << "姓名：" << name << endl;
+        cout << "学号：" << id << endl;
+    }
+};
+
+int main() {
+    Student student;
+    student.SetInfo();
+    student.ShowInfo();
+}
